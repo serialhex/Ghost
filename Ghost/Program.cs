0ff47b2990ff8ccc5f118400c67a8a0ad3a10e23
Justin Patera <serialhex@gmail.com>
2013-07-23 12:45:54 -0400

    Changed formatting; server works...

--- Ghost/Program.cs
+++ Ghost/Program.cs
@@ -5,12 +5,44 @@ using System.Text;
 using Zombie;
 using QBXMLRP2Lib;
 
-namespace Ghost
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Ghost {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("in main");
+            
+            // use this later... 
+            //Server srv = new Server();
+
+            string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><?qbxml version=\"6.0\"?><QBXML><QBXMLMsgsRq onError=\"stopOnError\"><CustomerQueryRq requestID=\"5001\" iterator=\"Start\"><MaxReturned>10</MaxReturned><IncludeRetElement>ListID</IncludeRetElement></CustomerQueryRq></QBXMLMsgsRq></QBXML>";
+            // make request
+            RequestProcessor2 rp = null;
+            string ticket = null;
+            string response = null;
+            try {
+                rp = new RequestProcessor2();
+                rp.OpenConnection("", "Test app");
+                ticket = rp.BeginSession("", QBFileMode.qbFileOpenDoNotCare);
+                response = rp.ProcessRequest(ticket, xml);
+
+            } catch (System.Runtime.InteropServices.COMException ex) {
+                Console.WriteLine("COM Error Description = " + ex.Message, "COM error");
+                Pause();
+                return;
+            } finally {
+                if (ticket != null) {
+                    rp.EndSession(ticket);
+                }
+                if (rp != null) {
+                    rp.CloseConnection();
+                }
+            };
+            Console.Write(response);
+            Pause();
+        }
+        
+        static void Pause() {
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
         }
     }
 }
--- Ghost/Server.cs
+++ Ghost/Server.cs
@@ -4,26 +4,26 @@ using System.Net.Sockets;
 using System.Threading;
 using System.Net;
 
-namespace Ghost
-{
-    class Server
-    {
+// code mostly stolen from:
+// http://tech.pro/tutorial/704/csharp-tutorial-simple-threaded-tcp-server
+
+namespace Ghost {
+    class Server {
         private TcpListener tcpListener;
         private Thread listenThread;
 
-        public Server()
-        {
-            this.tcpListener = new TcpListener(IPAddress.Any, 3000);
+        public Server() {
+            Console.WriteLine("making server");
+            this.tcpListener = new TcpListener(IPAddress.Loopback , 3000);
             this.listenThread = new Thread(new ThreadStart(ListenForClients));
             this.listenThread.Start();
         }
 
-        private void ListenForClients()
-        {
+        private void ListenForClients() {
+            Console.WriteLine("in listen for clients");
             this.tcpListener.Start();
 
-            while (true)
-            {
+            while (true) {
                 // blocks until a client has connected to the server
                 TcpClient client = this.tcpListener.AcceptTcpClient();
 
@@ -34,43 +34,42 @@ namespace Ghost
             }
         }
 
-        private void HandleClientComm(object client)
-        {
+        private void HandleClientComm(object client) {
+            Console.WriteLine("in handle client comm");
             TcpClient tcpClient = (TcpClient)client;
             NetworkStream clientStream = tcpClient.GetStream();
 
             byte[] message = new byte[4096];
             int bytesRead;
 
-            while (true)
-            {
+            while (true) {
                 bytesRead = 0;
 
-                try
-                {
+                try {
                     // blocks until a client sends a message
                     bytesRead = clientStream.Read(message, 0, 4096);
-                }
-                catch
-                {
+                } catch {
                     // a socket error has occured
                     break;
                 }
-
-                if (bytesRead == 0)
-                {
+                if (bytesRead == 0) {
                     // the client has disconnected from the server
                     break;
                 }
-
                 // message has successfully been received
                 ASCIIEncoding encoder = new ASCIIEncoding();
-                System.Diagnostics.Debug.WriteLine(encoder.GetString(message, 0, bytesRead));
+                Console.WriteLine(encoder.GetString(message, 0, bytesRead));
+
+                // send a message
+                Console.WriteLine("replying...\n");
+                ASCIIEncoding reply = new ASCIIEncoding();
+                byte[] buffer = reply.GetBytes("\nHello Client!\n\n");
+
+                clientStream.Write(buffer, 0, buffer.Length);
+                clientStream.Flush();
             }
 
             tcpClient.Close();
         }
-
-
     }
 }
